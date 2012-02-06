0f67eef3337a2066085c8f5a295f3deed153d7fe
Jeff Charles <oa5526339@gmail.com>
2012-02-06 12:43:30 +0000

    Updated test and remove log4j

--- src/test/java/com/wiley/frommers/digester/FeedServiceTest.java
+++ src/test/java/com/wiley/frommers/digester/FeedServiceTest.java
@@ -3,13 +3,17 @@
  */
 package com.wiley.frommers.digester;
 
-import org.apache.log4j.Logger;
+import java.util.logging.Logger;
+import java.util.logging.Level;
+
 
 import com.wiley.frommers.digester.domain.Location;
 import com.wiley.frommers.digester.domain.LocationNode;
 import com.wiley.frommers.digester.domain.MainSearchResult;
+import com.wiley.frommers.digester.domain.MediaSearchResult;
 import com.wiley.frommers.digester.domain.SearchResponse;
 import com.wiley.frommers.digester.query.MainSearchQuery;
+import com.wiley.frommers.digester.query.MediaSearchQuery;
 
 /**
  * Tests for digester
@@ -18,8 +22,8 @@ public class FeedServiceTest extends AbstractFeedTest {
     /**
      * Logger for this class
      */
-    private static final Logger LOGGER = Logger
-            .getLogger(FeedServiceTest.class);
+    private static final Logger LOGGER = Logger.getLogger(
+    		FeedServiceTest.class.getName());
 
     /** The destination service. */
     private FrommersDigester digester;
@@ -33,8 +37,8 @@ public class FeedServiceTest extends AbstractFeedTest {
     }
 
     public void testGetLocationById() throws FrommersFeedException {
-        if (LOGGER.isDebugEnabled()) {
-            LOGGER.debug("testGetLocationById() - start");
+        if (LOGGER.isLoggable(Level.FINE)) {
+            LOGGER.log(Level.FINE, "testGetLocationById() - start");
         }
 
         Location location = digester.getLocationById(PARIS_ID);
@@ -49,18 +53,15 @@ public class FeedServiceTest extends AbstractFeedTest {
             assertNotNull(temp);
             assertNotNull(temp.getName());
 
-            if (LOGGER.isDebugEnabled()) {
-                LOGGER.debug("testGetLocationById() - location : "
-                        + temp.getName());
+            if (LOGGER.isLoggable(Level.FINE)) {
+                LOGGER.log(Level.FINE, "testGetLocationById() - location : " + temp.getName());
             }
-
             temp = temp.getParent();
         }
 
-        if (LOGGER.isDebugEnabled()) {
-            LOGGER.debug("testGetLocationById() - end");
-
-        }
+        if (LOGGER.isLoggable(Level.FINE)) {
+            LOGGER.log(Level.FINE, "testGetLocationById() - end");
+        }    
     }
 
     public void testGetMainSearchSimple() throws FrommersFeedException {
@@ -70,164 +71,23 @@ public class FeedServiceTest extends AbstractFeedTest {
         assertTrue(resp.getCurrentPage() == 1);
         assertTrue(resp.getTotalResultCount() > 0);
     }
+    
+    public void testGetImagesForQueryRome() throws FrommersFeedException {
+        MediaSearchQuery query = new MediaSearchQuery();
+        
+        query.setQuery("rome");
+        LOGGER.log(Level.INFO, "Checking for photos matching query 'rome'");
+        
+        SearchResponse<MediaSearchResult> resp = digester.searchMedia(query);
+        assertNotNull(resp);        
+        assertTrue(resp.getCurrentPage() == 1);
+        assertTrue(resp.getTotalResultCount() > 0);
+        
+        // Loop through checking the thumb URLs are indeed null
+        for (MediaSearchResult media :resp.getResults()) {
+        	assertNull(media.getThumbnailUrl());
+        }
+    	
+    }
 
-    /*
-     * public void testGetEventByQuery() throws SispException { if
-     * (LOGGER.isDebugEnabled()) {
-     * LOGGER.debug("testGetEventByQuery() - start"); }
-     * 
-     * EventSearchQuery query = new EventSearchQuery(); // search paris event
-     * query.setLocationId(PARIS_ID.toString());
-     * 
-     * // page size query.setNPerPage(15);
-     * 
-     * SearchResponse<EventSearchResult> eventResult = digester
-     * .searchEvents(query);
-     * 
-     * assertNotNull(eventResult); assertTrue(eventResult.getTotalPageCount() >
-     * 0);
-     * 
-     * for (EventSearchResult event : eventResult.getResults()) {
-     * assertNotNull(event.getName()); assertNotNull(event.getId());
-     * 
-     * if (IOI_ID == null) IOI_ID = event.getId();
-     * 
-     * }
-     * 
-     * if (LOGGER.isDebugEnabled()) {
-     * LOGGER.debug("testGetEventByQuery() - end"); } }
-     * 
-     * public void testGetCategoriesByQuery() throws SispException { if
-     * (LOGGER.isDebugEnabled()) {
-     * LOGGER.debug("testGetCategoriesByQuery() - start"); }
-     * 
-     * AudienceInterestQuery query = new AudienceInterestQuery();
-     * 
-     * SearchResponse<AudienceInterestResult> categories = digester
-     * .searchAudienceInterests(query);
-     * 
-     * assertNotNull(categories); assertTrue(categories.getTotalPageCount() >
-     * 0);
-     * 
-     * if (LOGGER.isDebugEnabled()) {
-     * LOGGER.debug("testGetCategoriesByQuery() - end"); } }
-     * 
-     * public void testGetDestinationMenuByQuery() throws SispException { if
-     * (LOGGER.isDebugEnabled()) {
-     * LOGGER.debug("testGetDestinationMenuByQuery() - start"); }
-     * 
-     * DestinationMenuQuery query = new DestinationMenuQuery();
-     * 
-     * // destination menu of Paris query.setLocationId(PARIS_ID.toString());
-     * 
-     * DestinationMenu destinationMenu = digester
-     * .getDestinationMenuByQuery(query);
-     * 
-     * assertNotNull(destinationMenu); assertEquals(PARIS_ID,
-     * destinationMenu.getLocationId());
-     * 
-     * if (LOGGER.isDebugEnabled()) {
-     * LOGGER.debug("testGetDestinationMenuByQuery() - end"); } }
-     * 
-     * public void testGetItemOfInterestById() throws SispException { if
-     * (LOGGER.isDebugEnabled()) {
-     * LOGGER.debug("testGetItemOfInterestById() - start"); }
-     * 
-     * ItemOfInterest ioi = digester.getItemOfInterestById(IOI_ID);
-     * 
-     * assertNotNull(ioi.getName()); assertNotNull(ioi.getId());
-     * assertNotNull(ioi.getTypeCode());
-     * 
-     * if (LOGGER.isDebugEnabled()) {
-     * LOGGER.debug("testGetItemOfInterestById() - end"); } }
-     * 
-     * public void testGetLocationsByQuery() throws SispException { if
-     * (LOGGER.isDebugEnabled()) {
-     * LOGGER.debug("testGetLocationsByQuery() - start"); }
-     * 
-     * LocationSearchQuery query = new LocationSearchQuery();
-     * 
-     * query.setLocationType(COUNTRY);
-     * 
-     * SearchResponse<LocationSearchResult> locations = digester
-     * .searchLocations(query); assertNotNull(locations);
-     * 
-     * for (LocationSearchResult loc : locations.getResults()) {
-     * assertNotNull(loc.getName()); assertNotNull(loc.getId());
-     * 
-     * assertEquals(COUNTRY, loc.getType());
-     * 
-     * }
-     * 
-     * if (LOGGER.isDebugEnabled()) {
-     * LOGGER.debug("testGetLocationsByQuery() - end"); } }
-     * 
-     * public void testGetGuideStructureByQuery() throws SispException { if
-     * (LOGGER.isDebugEnabled()) {
-     * LOGGER.debug("testGetGuideStructureByQuery() - start"); }
-     * 
-     * GuideQuery query = new GuideQuery();
-     * query.setGuideStructureTypeId(GuideQuery.INTRODUCTION_TYPE_ID);
-     * query.setLocationId(PARIS_ID.toString());
-     * 
-     * GuideStructure guideStructure = digester
-     * .getGuideStructureByQuery(query);
-     * 
-     * assertNotNull(guideStructure);
-     * assertNotNull(guideStructure.getGuideStructureType());
-     * assertEquals(GuideQuery.INTRODUCTION_TYPE_ID, guideStructure
-     * .getGuideStructureType().getId().toString());
-     * 
-     * if (LOGGER.isDebugEnabled()) {
-     * LOGGER.debug("testGetGuideStructureByQuery() - end"); } }
-     * 
-     * public void testGetSildesShowByQuery() throws SispException { if
-     * (LOGGER.isDebugEnabled()) {
-     * LOGGER.debug("testGetSildesShowByQuery() - start"); }
-     * 
-     * SlideShowSearchQuery query = new SlideShowSearchQuery();
-     * query.setLocationId(PARIS_ID.toString());
-     * 
-     * SearchResponse<SlideshowSearchResult> slideshowsResult = digester
-     * .searchSildeshows(query);
-     * 
-     * assertNotNull(slideshowsResult);
-     * assertNotNull(slideshowsResult.getResults());
-     * 
-     * assertNotNull(slideshowsResult.getTotalResultCount());
-     * assertTrue(slideshowsResult.getTotalResultCount() > 0);
-     * 
-     * for (SlideshowSearchResult slide : slideshowsResult.getResults()) {
-     * assertNotNull(slide.getName());
-     * 
-     * Slideshow slidShow = digester.getSildeshowById(slide.getId());
-     * assertNotNull(slidShow); }
-     * 
-     * if (LOGGER.isDebugEnabled()) {
-     * LOGGER.debug("testGetSildesShowByQuery() - end"); } }
-     * 
-     * public void testGetPoisByQuery() throws SispException { if
-     * (LOGGER.isDebugEnabled()) { LOGGER.debug("testGetPoisByQuery() - start");
-     * }
-     * 
-     * PoiSearchQuery query = new PoiSearchQuery();
-     * query.setLocationId(PARIS_ID.toString()); query.setType("HOTEL");
-     * 
-     * SearchResponse<POISearchResult> searchResponse = digester
-     * .searchPois(query);
-     * 
-     * assertNotNull(searchResponse);
-     * assertNotNull(searchResponse.getResults());
-     * 
-     * for (POISearchResult poi : searchResponse.getResults()) {
-     * assertNotNull(poi.getName()); assertNotNull(poi.getType());
-     * assertEquals("HOTEL", poi.getType());
-     * 
-     * }
-     * 
-     * <<<<<<< HEAD if (LOGGER.isDebugEnabled()) {
-     * LOGGER.debug("testGetPoisByQuery() - end"); ======= if
-     * (logger.isDebugEnabled()) { logger.debug("testGetPoisByQuery() - end");
-     * >>>>>>> 4a717f0ec2a3db4e675ee3f7ea6312d05d773aaf } }
-     */
 }
